32587c31bb99bf2c6715619a014c143b62c18e60
Alt-F4-Twice <maximilian.sheerer@gmail.com>
2026-03-16 22:10:39 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "counter-api",
+  "version": "1.0.0",
+  "description": "Counter API with Owner/Admin panel, leaderboard, VPN detection, and Discord bot alerts",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "author": "Max",
+  "license": "MIT",
+  "dependencies": {
+    "bcrypt": "^5.1.0",
+    "cookie-parser": "^1.4.6",
+    "discord.js": "^14.12.1",
+    "express": "^4.18.2",
+    "express-rate-limit": "^7.0.0"
+  }
+}
