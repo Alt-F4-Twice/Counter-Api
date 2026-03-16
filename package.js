0db2b9f955660c224d3d0c16d61c9a4366fa471a
Alt-F4-Twice <maximilian.sheerer@gmail.com>
2026-03-16 22:21:09 +0000

    Update package.js

--- package.js
+++ package.js
@@ -1,18 +1,15 @@
 {
-  "name": "counter-api",
+  "name": "discord-api-bot",
   "version": "1.0.0",
-  "description": "Counter API with Owner/Admin panel, leaderboard, VPN detection, and Discord bot alerts",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
   },
-  "author": "Max",
-  "license": "MIT",
   "dependencies": {
+    "discord.js": "^15.0.0",
+    "express": "^4.18.2",
     "bcrypt": "^5.1.0",
     "cookie-parser": "^1.4.6",
-    "discord.js": "^14.12.1",
-    "express": "^4.18.2",
-    "express-rate-limit": "^7.0.0"
+    "express-rate-limit": "^6.7.0"
   }
 }
